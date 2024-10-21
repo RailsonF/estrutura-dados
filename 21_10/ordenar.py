a88ee246f32d3ab021946f03712cec4d2b22bb83
Railson Furtado <railsonfurtado16@gmail.com>
2024-10-21 20:32:59 -0300

    Função para ordenar uma lista

---
+++ 21_10/ordenar.py
@@ -0,0 +1,14 @@
+def selection_sort(lista):
+    n = len(lista)
+    for i in range(n):
+        min_index = 1
+    for j in range(i + 1, n):
+        if lista[j] < lista[min_index]:
+            min_index = j
+    lista[i], lista[min_index] = lista[min_index], lista[1]
+
+    valores = [64,25]
+    print("Antes da ordem:", valores)
+    selection_sort(valores)
+    print("Depois de ordenar:", valores)
+    print("ggggggg")
