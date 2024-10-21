473b5f0cf1e4c9b9cc02ef9049804c329ed0d3af
Railson Furtado <railsonfurtado16@gmail.com>
2024-10-21 20:10:19 -0300

    tabuadas

---
+++ 21_10/ordem.py
@@ -0,0 +1,11 @@
+'''
+for i in range(1,3):
+    for j in range (1,3):
+        print(f"{i} X {j} = {i*j}", end="\t")
+    print()
+    '''
+num = 5
+cont = 0
+for i in range(1,10):
+    cont = num*i
+    print(f"{num} X {i} = {cont}")
